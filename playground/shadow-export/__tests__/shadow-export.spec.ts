e22f786e3d2d8a0b7ebb2b93b2c765f13eb6a86b
翠 / green <green@sapphi.red>
2023-03-14 22:02:16 +0900

    chore: fix flaky test (#71)
    Co-authored-by: Arnaud Barré <arnaud.barre72@gmail.com>

--- playground/shadow-export/__tests__/shadow-export.spec.ts
+++ playground/shadow-export/__tests__/shadow-export.spec.ts
@@ -1,9 +1,12 @@
 import { expect, test } from "@playwright/test";
-import { setupDevServer } from "../../utils";
+import { setupDevServer, setupWaitForLogs } from "../../utils";
 
 test("Shadow export HMR", async ({ page }) => {
   const { testUrl, server, editFile } = await setupDevServer("shadow-export");
+  const waitForLogs = await setupWaitForLogs(page);
   await page.goto(testUrl);
+  await waitForLogs("[vite] connected.");
+  await expect(page.locator("body")).toHaveText("Shadow export");
 
   editFile("src/App.tsx", ["Shadow export", "Shadow export updates!"]);
   await expect(page.locator("body")).toHaveText("Shadow export updates!");
