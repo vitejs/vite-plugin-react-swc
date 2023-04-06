4d32784fbcf23b5b962164d3237619c932e4ed8d
Dan Tello <greypants@users.noreply.github.com>
2023-04-06 13:08:32 -0400

    configure mdx plugin so build script works
    Adds solution referenced here: https://github.com/vitejs/vite-plugin-react/blob/main/playground/mdx/vite.config.ts#L8
    
    Without it, the `build` script throws:
    
    ```
    [vite:react-swc]
      × Expected ident
    ```

--- playground/mdx/vite.config.ts
+++ playground/mdx/vite.config.ts
@@ -3,5 +3,5 @@ import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [mdx(), react()],
+  plugins: [{ enforce: "pre", ...mdx() }, react()],
 });
