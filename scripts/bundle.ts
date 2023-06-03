8e311c07fec8a6de006c908951ec130ff6ea0e26
Arnaud Barré <arnaud.barre@carbometrix.com>
2023-06-03 12:12:06 +0200

    chore: tsx -> tnode

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -2,7 +2,7 @@ import { rmSync, writeFileSync, copyFileSync } from "node:fs";
 import { execSync } from "node:child_process";
 import { build, BuildOptions, context } from "esbuild";
 
-import * as packageJSON from "../package.json";
+import packageJSON from "../package.json";
 
 const dev = process.argv.includes("--dev");
 
