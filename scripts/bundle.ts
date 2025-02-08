ff29dc874281b2875ec6dc32bc07b5f5b55d7b6c
Arnaud Barré <arnaud.barre@carbometrix.com>
2025-02-08 23:44:23 +0100

    Bump SWC plugins

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,6 +1,6 @@
 import { rmSync, writeFileSync, copyFileSync } from "node:fs";
 import { execSync } from "node:child_process";
-import { build, BuildOptions, context } from "esbuild";
+import { build, type BuildOptions, context } from "esbuild";
 
 import packageJSON from "../package.json";
 
